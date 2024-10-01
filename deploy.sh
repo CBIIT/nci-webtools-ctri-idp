6173d898d2b61f2a0b3cda2366cb63ad12be78c9
Brian Park <park-brian@users.noreply.github.com>
2024-10-01 05:05:22 -0400

    popd after deploying stack

--- deploy.sh
+++ deploy.sh
@@ -3,10 +3,9 @@ set -ex
 
 pushd infrastructure
 npm install
-
-# todo: remove hosted zone
-cdk deploy ctri-idp-$TIER-route53-hosted-zone --require-approval never
+cdk deploy ctri-idp-$TIER-route53-hosted-zone --require-approval never # todo: remove this line
 cdk deploy ctri-idp-$TIER-ecr-repository --require-approval never
+popd
 
 export ECR_REGISTRY=${AWS_ACCOUNT_ID}.dkr.ecr.us-east-1.amazonaws.com
 export ECR_REPOSITORY=ctri-idp-$TIER
@@ -14,8 +13,8 @@ export ECR_REPOSITORY=ctri-idp-$TIER
 export CLIENT_IMAGE=$ECR_REGISTRY/$ECR_REPOSITORY:client-$GITHUB_SHA
 export SERVER_IMAGE=$ECR_REGISTRY/$ECR_REPOSITORY:server-$GITHUB_SHA
 
-export CLIENT_IMAGE_LATEST=$ECR_REGISTRY/$ECR_REPOSITORY:ui-latest
-export SERVER_IMAGE_LATEST=$ECR_REGISTRY/$ECR_REPOSITORY:api-latest
+export CLIENT_IMAGE_LATEST=$ECR_REGISTRY/$ECR_REPOSITORY:client-latest
+export SERVER_IMAGE_LATEST=$ECR_REGISTRY/$ECR_REPOSITORY:server-latest
 
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
 
@@ -27,4 +26,6 @@ docker build -t $SERVER_IMAGE -t $SERVER_IMAGE_LATEST -f server/Dockerfile .
 docker push $SERVER_IMAGE 
 docker push $SERVER_IMAGE_LATEST
 
+pushd infrastructure
 cdk deploy ctri-idp-$TIER-ecs-service --require-approval never
+popd
