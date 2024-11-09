75e4fb4fb38bef74c4e0abf169996cca1d224527
Brian Park <park-brian@users.noreply.github.com>
2024-11-08 23:19:39 -0500

    add default parameters

--- server/services/inference.js
+++ server/services/inference.js
@@ -1,15 +1,27 @@
 import { BedrockRuntimeClient, ConverseCommand } from "@aws-sdk/client-bedrock-runtime"; // ES Modules import
 import { parseDocument } from "./parsers.js";
 
+// Default model ID
+export const DEFAULT_MODEL_ID = process.env.DEFAULT_MODEL_ID || "anthropic.claude-3-5-sonnet-20240620-v1:0"
+
 /**
  * Run a model with the given messages.
  * @param {string} modelId
- * @param {{role: "user" | "assistant" | "system", content: string}[]} messages
+ * @param {{role: "user" | "assistant" | "system", content: string}[] | string} messages
  * @returns
  */
-export async function runModel(modelId, messages) {
+export async function runModel(modelId = DEFAULT_MODEL_ID, messages = []) {
+  if (!messages || messages?.length === 0) {
+    return null;
+  }
+
+  if (typeof messages === "string") {
+    messages = [{ role: "user", content: [{ text: messages }] }];
+  }
+
   const client = new BedrockRuntimeClient();
   const input = { modelId, messages };
+  
   const command = new ConverseCommand(input);
   const response = await client.send(command);
   return response;
@@ -22,10 +34,10 @@ export async function runModel(modelId, messages) {
  * @param {{originalname: string, buffer: Buffer, mimetype: string}} document
  * @returns {Promise<any>}
  */
-export async function processDocument(modelId, prompt, document) {
+export async function processDocument(modelId, prompt, document = null) {
   const startTime = Date.now();
   try {
-    const text = await parseDocument(document.buffer, document.mimetype);
+    const text = document ? await parseDocument(document.buffer, document.mimetype) : "";
     const userMessage = prompt + "\n" + text;
     const messages = [{ role: "user", content: [{ text: userMessage }] }];
     const results = await runModel(modelId, messages);
